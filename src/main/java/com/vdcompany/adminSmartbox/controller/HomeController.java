016a88f59977f6f56449465afb557ecee4c6670f
parkng5 <parkng5@vdcompany.co.kr>
2021-06-10 18:50:38 +0900

    2021.06.10.   - db conn. 접속 정보 기존 아마존 -> IDC test db

--- src/main/java/com/vdcompany/adminSmartbox/controller/HomeController.java
+++ src/main/java/com/vdcompany/adminSmartbox/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.vdcompany.adminSmartbox.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.vdcompany.adminSmartbox.bean.web.menu.LeftMenuListVO;
 import com.vdcompany.adminSmartbox.bean.web.menu.LeftMenuSubVO;
 import com.vdcompany.adminSmartbox.bean.web.menu.LeftMenuVO;
 import org.slf4j.Logger;
@@ -27,37 +30,32 @@ public class HomeController {
 	CategoryService cateService;*/
 
 	Logger logger = LoggerFactory.getLogger(this.getClass());
-
+	String pageTitle = "홈";
+	String menuListJson = "{\n" +
+			"   \"leftMenuList\":[\n" +
+			"      {\n" +
+			"         \"menu_name\":\"주요현황\",\n" +
+			"         \"menu_icon\":\"pe-7s-display2\",\n" +
+			"         \"link_url\":\"#\",\n" +
+			"         \"leftMenuSub\":null\n" +
+			"      }\n" +
+			"   ]\n" +
+			"}";
+	//
 	@RequestMapping("/dashboard")
 	private ModelAndView mainPage(Model model, HttpServletRequest request) {
-		String pageTitle = "홈";
 		String url = "/home/dashboard";
-		logger.info("[WEB][webController][mainPage] url:"+url);
 		ModelAndView mav = new ModelAndView(url);
 
 		Map<String, Object> pageinfo = new HashMap<>();
 		pageinfo.put("pageTitle", pageTitle);
 		pageinfo.put("date", LocalDateTime.now());
 
-
-		List<LeftMenuVO> leftMenu = new ArrayList<>();
-		LeftMenuVO leftMenuVO = new LeftMenuVO();
-		leftMenuVO.setMenu_name("주요현황");
-		leftMenuVO.setMenu_icon("pe-7s-display2");
-		leftMenuVO.setLink_url("/home/dashboard");
-
-		/*List<LeftMenuSubVO> leftMenuSubVOList = new ArrayList<>();
-		LeftMenuSubVO leftMenuSubVO = new LeftMenuSubVO();
-		leftMenuSubVO.setMenu_name("주요현황");
-		leftMenuSubVO.setLink_url("/home/dashboard");
-		leftMenuSubVOList.add(leftMenuSubVO);
-
-		leftMenuVO.setLeftMenuSub(leftMenuSubVOList);*/
-
-		leftMenu.add(leftMenuVO);
+		LeftMenuListVO leftMenuListVO = new Gson().fromJson(menuListJson, LeftMenuListVO.class);
+		logger.info("json:"+new GsonBuilder().setPrettyPrinting().create().toJson(leftMenuListVO));
 
 		mav.addObject("pageInfo", pageinfo);
-		mav.addObject("leftMenuInfo", leftMenu);
+		mav.addObject("leftMenuInfo", leftMenuListVO);
 
 		return mav;
 	}
